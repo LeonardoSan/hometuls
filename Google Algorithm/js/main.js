1761f9de6e7252aaa79aff6c70d2647ce4965510
Leonardo Sanabria Hoyos <leonardosanh@gmail.com>
2019-02-21 13:22:44 -0500

    minor changes in google algorithm

--- Google Algorithm/js/main.js
+++ Google Algorithm/js/main.js
@@ -3,8 +3,6 @@
 var google = [
 	['g', 'G'],
 	['o', 0, 'O', '()', '[]', '<>'],
-	['o', 0, 'O', '()', '[]', '<>'],
-	['g', 'G'],
 	['l', 'L', 'I'],
 	['e', 'E', 3]
 ];
@@ -13,11 +11,11 @@ var words = [];
 
 for (var i = 0; i < google[0].length; i++) {
 	for (var j = 0; j < google[1].length; j++) {
-		for (var k = 0; k < google[2].length; k++) {
-			for (var l = 0; l < google[3].length; l++) {
-				for (var m = 0; m < google[4].length; m++) {
-					for (var n = 0; n < google[5].length; n++) {
-						words.push(google[0][i] + google[1][j] + google[2][k] + google[3][l] + google[4][m] + google[5][n]);
+		for (var k = 0; k < google[1].length; k++) {
+			for (var l = 0; l < google[0].length; l++) {
+				for (var m = 0; m < google[2].length; m++) {
+					for (var n = 0; n < google[3].length; n++) {
+						words.push(google[0][i] + google[1][j] + google[1][k] + google[0][l] + google[2][m] + google[3][n]);
 					}
 				}
 			}
